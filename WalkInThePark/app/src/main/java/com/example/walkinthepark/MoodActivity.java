ebc65739a30fb03a82a92a02923e6cf1243c70c0
Ramulho <diogorduarte00@gmail.com>
2021-11-23 14:27:19 +0000

    mood fragments

--- WalkInThePark/app/src/main/java/com/example/walkinthepark/MoodActivity.java
+++ WalkInThePark/app/src/main/java/com/example/walkinthepark/MoodActivity.java
@@ -1,14 +1,38 @@
 package com.example.walkinthepark;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class MoodActivity extends AppCompatActivity {
 
+    public static Fragment moodsFragment;
+    public static Fragment newMoodFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mood);
+
+        if (newMoodFragment== null){
+            newMoodFragment = new NewMoodFragment();
+        }
+
+        if (moodsFragment== null){
+            moodsFragment = new MoodsFragment();
+        }
+        replaceFragment(moodsFragment);
+    }
+
+    public void replaceFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.flFragment, fragment);
+        fragmentTransaction.commit();
+
     }
 }
