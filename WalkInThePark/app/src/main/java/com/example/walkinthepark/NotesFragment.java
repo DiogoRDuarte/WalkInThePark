f22083c22bbc1d05360a6bd8320d089f181b713c
jjaneiro1990 <jjaneiro1990@gmail.com>
2021-12-09 16:03:20 +0000

    Adicao Firebase Notas

--- WalkInThePark/app/src/main/java/com/example/walkinthepark/NotesFragment.java
+++ WalkInThePark/app/src/main/java/com/example/walkinthepark/NotesFragment.java
@@ -7,7 +7,14 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -16,14 +23,33 @@ public class NotesFragment extends Fragment {
     static View notesView;
     private TextView notas;
     private ArrayList<Note> listaNotas;
+    private DatabaseReference refNotes;
+    private FirebaseDatabase db;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         notesView = inflater.inflate(R.layout.fragment_notes, container, false);
+        db = FirebaseDatabase.getInstance("https://walk-in-the-park---cm-default-rtdb.firebaseio.com/");
+        refNotes = db.getReference("Note");
 
+        refNotes.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot ds : snapshot.getChildren()){
+                    String s = ds.child("titulo").getValue().toString();
+                    String s1 = ds.child("mensagem").getValue().toString();
+                    listaNotas.add(new Note(s,s1));
+                }
+            }
 
-        listaNotas = ((NotesActivity) getActivity()).getListaNotas();
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        //listaNotas = ((NotesActivity) getActivity()).getListaNotas();
         notas = notesView.findViewById(R.id.listaNotas);
 
         StringBuilder m = new StringBuilder("");
@@ -33,7 +59,7 @@ public class NotesFragment extends Fragment {
         }
 
         if(m.toString().equals("")){
-            notas.setText("Não Existem Lembretes!");
+            notas.setText("Não Existem Notas!");
         }else
             notas.setText(m.toString());
 
