edc6fecfc5613580160160fc8cf7f054bba5df66
jjaneiro1990 <jjaneiro1990@gmail.com>
2021-12-21 18:44:24 +0000

    Update ProfHomeActivity.java

--- WalkInThePark/app/src/main/java/com/example/walkinthepark/ProfHomeActivity.java
+++ WalkInThePark/app/src/main/java/com/example/walkinthepark/ProfHomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.walkinthepark;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,8 +8,20 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicMarkableReference;
 
 public class ProfHomeActivity extends AppCompatActivity {
+
+    private DatabaseReference myRef;
+    private FirebaseDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +32,32 @@ public class ProfHomeActivity extends AppCompatActivity {
         MaterialButton criarVideo = findViewById(R.id.adicionarVideo);
         MaterialButton verPacientes = findViewById(R.id.verPacientes);
 
+        db = FirebaseDatabase.getInstance("https://walk-in-the-park---cm-default-rtdb.firebaseio.com/");
+        myRef = db.getReference("User");
+        Map m = new HashMap<String,Map>();
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for(DataSnapshot ds : snapshot.getChildren()){
+                    String sEmail = ds.child("email").getValue().toString();
+                    String sPassword = ds.child("password").getValue().toString();
+                    String sNome = ds.child("nome").getValue().toString();
+                    String sFisio = ds.child("fisioID").getValue().toString();
+                    boolean pac = ds.child("paciente").getValue(Boolean.class);
+                    User u = new User(sNome, sEmail,sPassword,sFisio,pac);
+                    if(pac){
+                        m.put(sEmail,u.toMap());
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
         // BUTTONS
         /*verVideos.setOnClickListener(new View.OnClickListener() {
             @Override
