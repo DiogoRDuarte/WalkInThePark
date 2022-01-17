cff52d4f4e9bc9ca6e5b2ca766d013affb8f9e26
fc52771 <fc52771@alunos.fc.ul.pt>
2022-01-17 22:35:59 +0000

    update layout

--- WalkInThePark/smartwatch/src/main/java/com/example/smartwatch/MainActivity.java
+++ WalkInThePark/smartwatch/src/main/java/com/example/smartwatch/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.smartwatch;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
@@ -8,9 +12,11 @@ import android.widget.Toast;
 import androidx.wear.widget.WearableLinearLayoutManager;
 import androidx.wear.widget.WearableRecyclerView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity  {
 
     private WearableRecyclerView wearableRecyclerView;
 
@@ -21,11 +27,11 @@ public class MainActivity extends Activity {
 
         wearableRecyclerView = findViewById(R.id.recyclerView);
         wearableRecyclerView.setEdgeItemsCenteringEnabled(true);
-        wearableRecyclerView.setLayoutManager(new WearableLinearLayoutManager(this));
+        /*wearableRecyclerView.setLayoutManager(new WearableLinearLayoutManager(this));*/
 
-       /* CustomScrollingLayoutCallback customScrollingLayoutCallback = new CustomScrollingLayoutCallback();
+        CustomScrollingLayoutCallback customScrollingLayoutCallback = new CustomScrollingLayoutCallback();
         wearableRecyclerView.setLayoutManager(new WearableLinearLayoutManager(this, customScrollingLayoutCallback));
-        wearableRecyclerView.setCircularScrollingGestureEnabled(true);*/
+        wearableRecyclerView.setCircularScrollingGestureEnabled(true);
 
         ArrayList<MenuItem> menuItems = new ArrayList<>();
         menuItems.add(new MenuItem(R.drawable.pencil, "Notas"));
@@ -59,6 +65,16 @@ public class MainActivity extends Activity {
             }
         }));
 
+        /*// SENSORES
+        SensorManager mSensorManager = ((SensorManager)getSystemService(SENSOR_SERVICE));
+        Sensor mHeartRateSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+        Sensor mStepCountSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        Sensor mStepDetectSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+
+        mSensorManager.registerListener(this, mHeartRateSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mStepCountSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mStepDetectSensor, SensorManager.SENSOR_DELAY_NORMAL);*/
+
     }
 
     public void action_1(){
@@ -91,4 +107,33 @@ public class MainActivity extends Activity {
         Toast.makeText(getApplicationContext(), "cancel", Toast.LENGTH_SHORT);
     }
 
+    /*// hora para guardar dados
+    private String currentTimeStr() {
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
+        return df.format(c.getTime());
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_HEART_RATE) {
+            String msg = "" + (int)sensorEvent.values[0];
+            mTextViewHeart.setText(msg);
+        }
+        else if (sensorEvent.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            String msg = "Count: " + (int)sensorEvent.values[0];
+            mTextViewStepCount.setText(msg);
+        }
+        else if (sensorEvent.sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
+            String msg = "Detected at " + currentTimeStr();
+            mTextViewStepDetect.setText(msg);
+        }
+        else
+            Toast.makeText(getApplicationContext(), "Sensor inválido", Toast.LENGTH_SHORT);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }*/
 }
