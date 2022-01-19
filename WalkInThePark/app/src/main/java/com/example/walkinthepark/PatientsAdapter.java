45da85a2cbcd22936322ed962aea0aa461de82eb
Ramulho <diogorduarte00@gmail.com>
2022-01-19 16:50:26 +0000

    Revert "Pacientes Clicaveis"
    This reverts commit 9cf2e043e2cb0cbccc64a27a50c2416deaa5e0d9.

--- WalkInThePark/app/src/main/java/com/example/walkinthepark/PatientsAdapter.java
+++ WalkInThePark/app/src/main/java/com/example/walkinthepark/PatientsAdapter.java
@@ -27,7 +27,7 @@ public class PatientsAdapter extends RecyclerView.Adapter<PatientsAdapter.ViewHo
 
     private RecyclerViewListener listener;
 
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
         public TextView pacNameTextView;
         public ImageButton editButton;
@@ -40,15 +40,6 @@ public class PatientsAdapter extends RecyclerView.Adapter<PatientsAdapter.ViewHo
             editButton = (ImageButton) itemView.findViewById(R.id.editButton);
             statisticButton = (ImageButton) itemView.findViewById(R.id.statisticButton);
 
-            itemView.setOnClickListener(this);
-
-        }
-
-        @Override
-        public void onClick(View v) {
-            if(listener != null && getAdapterPosition() != RecyclerView.NO_POSITION){
-                listener.onClick(v, getAdapterPosition());
-            }
         }
     }
 
@@ -63,7 +54,7 @@ public class PatientsAdapter extends RecyclerView.Adapter<PatientsAdapter.ViewHo
         Context context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
         View userView = inflater.inflate(R.layout.single_paciente,parent,false);
-        PatientsAdapter.ViewHolder viewHolder = new PatientsAdapter.ViewHolder(userView);
+        ViewHolder viewHolder = new ViewHolder(userView);
         return viewHolder;
     }
 
